e3b31b3c1bac4daa245e1238ade7d7fa69235882
SIMPLY-PHIL <41239622+SIMPLY-PHIL@users.noreply.github.com>
2021-02-15 15:46:27 -0500

    Add files via upload

---
+++ array_pointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// fill the array
+void fill_array(int * arr1, int len);
+
+int main(int argc, char * * argv)
+
+{
+
+  
+
+
+
+}
+void fill_array(int * arr1, int len)
+{
+
+}
+
+
+  
